97af05b4631fed5801d6fbd6afe44579694eab2d
Adam Seid <adam@amurgroup.ca>
2026-02-12 16:16:19 -0800

    updated mobile menu

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { Menu, X } from "lucide-react";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -66,13 +67,30 @@ export default function Navbar() {
                         <img src="/icons/phone.svg" alt="Phone Icon" className="w-3 object-contain" />
                         </button>
                     </div>
-                </div>
+                </div>``
 
                 {/* MOBILE BUTTON */}
                 <div className="md:hidden">
-                    <button onClick={() => setMenuOpen(prev => !prev)}>
-                        <img src="/icons/mobile-menu.svg" className="w-3" />
-                    </button>
+                    <div className="
+                        z-[1]
+                        rounded-xl
+                        h-full 
+                        p-[1.5px] 
+                        bg-[radial-gradient(102.5%_350.82%_at_0%_81.25%,#AA5BFF_0%,#9747FF_52.42%,#7C0EDD_100%)]
+                        shadow-[0_0_14px_0_#FFFFFF4D_inset,0_0_30.3px_0_#9747FF66]
+                    ">
+                        <button onClick={() => setMenuOpen(prev => !prev)} className="
+                            flex items-center gap-3 justify-center
+                            text-white font-semibold
+                            rounded-xl
+                            bg-[radial-gradient(102.5%_350.82%_at_0%_81.25%,#AA5BFF_0%,#9747FF_52.42%,#7C0EDD_100%)]
+                            p-[9.91px]
+                            w-full h-full
+                            cursor-pointer
+                        ">
+                            {menuOpen ? <X size={20} /> : <Menu size={20} />}
+                        </button>
+                    </div>
                 </div>
             </div>
         </nav>
@@ -83,13 +101,13 @@ export default function Navbar() {
                 menuOpen ? "translate-y-0" : "-translate-y-full"
             }`}
         >
-            <div className="flex flex-row items-center justify-end gap-8">
+            <div className="flex flex-col items-center justify-start gap-8 pt-[55px]">
                 {/* Links */}
-                <div className="flex flex-row items-center gap-[17]">
-                    <a href="#" className="text-white p-2.5">Home</a>
-                    <a href="#" className="text-white p-2.5">Result</a>
-                    <a href="#" className="text-white p-2.5">Services</a>
-                    <a href="#" className="text-white p-2.5">Reviews</a>
+                <div className="flex flex-col items-center justify-start gap-[17]">
+                    <a href="#" className="text-white p-2.5 font-segoe font-normal text-[14px] leading-[100%] tracking-[0px]">Home</a>
+                    <a href="#" className="text-white p-2.5 font-segoe font-normal text-[14px] leading-[100%] tracking-[0px]">Result</a>
+                    <a href="#" className="text-white p-2.5 font-segoe font-normal text-[14px] leading-[100%] tracking-[0px]">Services</a>
+                    <a href="#" className="text-white p-2.5 font-segoe font-normal text-[14px] leading-[100%] tracking-[0px]">Reviews</a>
                 </div>
 
                 {/* Contact Button */}
